07fe88e8f8c476003c9100d7dc62f97535ee8488
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-09-08 14:09:43 +0100

    fontawesome & jQuery and other scripts added

--- header.php
+++ header.php
@@ -16,6 +16,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script src="https://kit.fontawesome.com/59e8cbdc1f.js" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<script>
+	$(document).ready(function(){
+		$(".menu-button").click(function(){
+			$(".menu-items").toggleClass('display');
+		});
+		});
+	</script>
+
 
 	<?php wp_head(); ?>
 </head>
